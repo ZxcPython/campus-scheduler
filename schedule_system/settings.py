205cb017f53a9211c5bc4557c011a8197a4e1440
DevMe001 <dev.me28@gmail.com>
2024-01-22 16:47:50 +0800

    dyango deployed

--- schedule_system/settings.py
+++ schedule_system/settings.py
@@ -98,6 +98,10 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+    {
+        'NAME': 'django.contrib.staticfiles.middleware.StaticFilesMiddleware',
+        
+    }
 ]
 
 
@@ -119,7 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Location for collectstatic to gather static files for deployment
-STATIC_ROOT = '../main/static'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
