03b031b2cc7b2ecb376e081c866c2e7fab45c768
DevMe001 <dev.me28@gmail.com>
2024-01-22 16:42:52 +0800

    dyango deployed

--- schedule_system/wsgi.py
+++ schedule_system/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Scheduler.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'schedule_system.settings')
+
 
 application = get_wsgi_application()
